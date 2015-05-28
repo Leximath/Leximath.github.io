c5637b706f07208411d834db49c28cc074da5ceb
Flávio Codeço Coelho <fccoelho@gmail.com>
2015-05-28 16:42:01 -0300

    removi Light_old

--- src/files/apresentacoes/Light_old/graficos/lisa/gen_csv.sh
+++
@@ -1,5 +0,0 @@
-psql -h dir-rj sap_light -A -F "," < SQLs/corte.sql       > CSVs/corte.csv   
-psql -h dir-rj sap_light -A -F "," < SQLs/negativacao.sql > CSVs/negativacao.csv  
-psql -h dir-rj sap_light -A -F "," < SQLs/reclamacao.sql  > CSVs/reclamacao.csv 
-psql -h dir-rj sap_light -A -F "," < SQLs/toi.sql         > CSVs/toi.csv 
-psql -h dir-rj sap_light -A -F "," < SQLs/processo.sql        > CSVs/processo.csv 
--- src/files/apresentacoes/Light_old/visualizacoes-bk/7/convert.sh
+++
@@ -1,5 +0,0 @@
-for i in *.$1
-do
-    filename=$(basename "$i" $1)
-    python csv2pairs.py $i 0 1 ' '  > $filename
-done
